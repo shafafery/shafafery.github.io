b6fe50692c937515b900841703a8da0fd66f5ecf
Shafa Fery Az-Zahra <120653126+shafafery@users.noreply.github.com>
2025-11-02 19:34:02 +0700

    Update index.js

--- css/index.js
+++ css/index.js
@@ -1,15 +1,17 @@
 <?php
-/
- 
-Front to the WordPress application. This file doesn't do anything, but loads
-wp-page.php which does and tells WordPress to load the theme.*
-@package WordPress (modified)*/
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-/
- 
-Tells WordPress to load the WordPress theme and output it.*
-@var bool*/
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
 define( 'WP_USE_THEMES', true );
 
-/** Load custom front loader instead of wp-blog-header.php */
-require DIR . '/wp-page.php';
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-page.php';
